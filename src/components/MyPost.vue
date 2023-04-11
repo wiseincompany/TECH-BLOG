241805833db1d351e416e9bf43433d8d18a1be4f
Returnmakdo <cldud970@naver.com>
2023-04-11 15:39:54 +0900

    ver 1.0.1

--- src/components/MyPost.vue
+++ src/components/MyPost.vue
@@ -5,16 +5,16 @@
       max-width="330"
       min-width="330">
       <v-img
-        :src="require(`@/assets/짱구.jpg`)"
+        :src="require(`@/assets/2023/card_title05.jpg`)"
         height="240px"
         cover></v-img>
       <v-card-title>안녕하세요!</v-card-title>
-      <v-card-subtitle class="subTitle">짱구에오</v-card-subtitle>
+      <v-card-subtitle class="subTitle">안녕하세요</v-card-subtitle>
       <v-card-actions>
         <v-btn
           color="purple-lighten-2"
           variant="text">
-          Click!
+          Tech
         </v-btn>
         <v-spacer></v-spacer>
       </v-card-actions>
@@ -24,16 +24,17 @@
       max-width="330"
       min-width="330">
       <v-img
-        :src="require(`@/assets/짱구.jpg`)"
+        :src="require(`@/assets/2023/card_title04.jpg`)"
         height="240px"
         cover></v-img>
+
       <v-card-title>안녕하세요!</v-card-title>
-      <v-card-subtitle class="subTitle">짱구에오</v-card-subtitle>
+      <v-card-subtitle class="subTitle">안녕하세요</v-card-subtitle>
       <v-card-actions>
         <v-btn
           color="purple-lighten-2"
           variant="text">
-          Click!
+          Tech
         </v-btn>
         <v-spacer></v-spacer>
       </v-card-actions>
@@ -43,16 +44,16 @@
       max-width="330"
       min-width="330">
       <v-img
-        :src="require(`@/assets/짱구.jpg`)"
+        :src="require(`@/assets/2023/card_title03.jpg`)"
         height="240px"
         cover></v-img>
-      <v-card-title>안녕하세요!</v-card-title>
-      <v-card-subtitle class="subTitle">짱구에오</v-card-subtitle>
+      <v-card-title>와이즈인 컴퍼니</v-card-title>
+      <v-card-subtitle class="subTitle">2023.04.15</v-card-subtitle>
       <v-card-actions>
         <v-btn
           color="purple-lighten-2"
           variant="text">
-          Click!
+          Tech
         </v-btn>
         <v-spacer></v-spacer>
       </v-card-actions>
@@ -62,16 +63,16 @@
       max-width="330"
       min-width="330">
       <v-img
-        :src="require(`@/assets/짱구.jpg`)"
+        :src="require(`@/assets/2023/card_title02.jpg`)"
         height="240px"
         cover></v-img>
-      <v-card-title>안녕하세요!</v-card-title>
-      <v-card-subtitle class="subTitle">짱구에오</v-card-subtitle>
+      <v-card-title>개발팀을 소개합니다!</v-card-title>
+      <v-card-subtitle class="subTitle">2023.04.15</v-card-subtitle>
       <v-card-actions>
         <v-btn
           color="purple-lighten-2"
           variant="text">
-          Click!
+          Tech
         </v-btn>
         <v-spacer></v-spacer>
       </v-card-actions>
@@ -81,16 +82,16 @@
       max-width="330"
       min-width="330">
       <v-img
-        :src="require(`@/assets/짱구.jpg`)"
+        :src="require(`@/assets/2023/card_title01.jpg`)"
         height="240px"
         cover></v-img>
-      <v-card-title>안녕하세요!</v-card-title>
-      <v-card-subtitle class="subTitle">짱구에오</v-card-subtitle>
+      <v-card-title>와이즈인 개발 블로그를 소개합니다!</v-card-title>
+      <v-card-subtitle class="subTitle">2023. 04. 11</v-card-subtitle>
       <v-card-actions>
         <v-btn
           color="purple-lighten-2"
           variant="text">
-          Click!
+          Tech
         </v-btn>
         <v-spacer></v-spacer>
       </v-card-actions>
