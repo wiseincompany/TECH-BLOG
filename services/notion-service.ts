4e589bfcb629e81f06e4646f76e6991d3fb1cfad
GitHubJIYEON <jiyeon2954@gmail.com>
2023-04-14 08:53:31 +0900

    불필요 파일 삭제

--- services/notion-service.ts
+++ services/notion-service.ts
@@ -1,18 +1,18 @@
-import {Client} from '@notionhq/client';
-import {BlogPost, PostPage} from '../@types/schema';
-import {NotionToMarkdown} from 'notion-to-md';
+import { Client } from '@notionhq/client'
+import { BlogPost, PostPage } from '../@types/schema'
+import { NotionToMarkdown } from 'notion-to-md'
 
 export default class NotionService {
-  client: Client;
-  n2m: NotionToMarkdown;
+  client: Client
+  n2m: NotionToMarkdown
 
   constructor() {
-    this.client = new Client({auth: process.env.NOTION_ACCESS_TOKEN});
-    this.n2m = new NotionToMarkdown({notionClient: this.client});
+    this.client = new Client({ auth: process.env.NOTION_ACCESS_TOKEN })
+    this.n2m = new NotionToMarkdown({ notionClient: this.client })
   }
 
   async getPublishedBlogPosts(): Promise<BlogPost[]> {
-    const database = process.env.NOTION_BLOG_DATABASE_ID ?? '';
+    const database = process.env.NOTION_BLOG_DATABASE_ID ?? ''
     // 글 리스트
     const response = await this.client.databases.query({
       database_id: database,
@@ -22,17 +22,17 @@ export default class NotionService {
           direction: 'descending',
         },
       ],
-    });
+    })
 
     return response.results.map((res) => {
-      return NotionService.pageToPostTransformer(res);
-    });
+      return NotionService.pageToPostTransformer(res)
+    })
   }
 
   async getSingleBlogPost(slug: string): Promise<PostPage> {
-    let post, markdown;
+    let post, markdown
 
-    const database = process.env.NOTION_BLOG_DATABASE_ID ?? '';
+    const database = process.env.NOTION_BLOG_DATABASE_ID ?? ''
     // 글 리스트
     const response = await this.client.databases.query({
       database_id: database,
@@ -51,35 +51,35 @@ export default class NotionService {
           direction: 'descending',
         },
       ],
-    });
+    })
 
     if (!response.results[0]) {
-      throw 'No results available';
+      throw 'No results available'
     }
 
     // 노션에서 가져온 페이지
-    const page = response.results[0];
+    const page = response.results[0]
 
-    const mdBlocks = await this.n2m.pageToMarkdown(page.id);
-    markdown = this.n2m.toMarkdownString(mdBlocks);
-    post = NotionService.pageToPostTransformer(page);
+    const mdBlocks = await this.n2m.pageToMarkdown(page.id)
+    markdown = this.n2m.toMarkdownString(mdBlocks)
+    post = NotionService.pageToPostTransformer(page)
     return {
       post,
       markdown,
-    };
+    }
   }
 
   private static pageToPostTransformer(page: any): BlogPost {
-    let cover = page.cover;
+    let cover = page.cover
     switch (cover.type) {
       case 'file':
-        cover = page.cover.file;
-        break;
+        cover = page.cover.file
+        break
       case 'external':
-        cover = page.cover.external.url;
-        break;
+        cover = page.cover.external.url
+        break
       default:
-        cover = '';
+        cover = ''
     }
 
     return {
@@ -90,6 +90,6 @@ export default class NotionService {
       description: page.properties.Description.rich_text[0]?.plain_text,
       date: page.properties.Updated?.last_edited_time,
       slug: page.properties.Slug?.formula.string,
-    };
+    }
   }
 }
