ec0a059e1471df3e6d8aa15c154d5dc1a5692813
Returnmakdo <cldud970@naver.com>
2023-04-14 08:56:06 +0900

    prettier 설정 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,48 @@
-import {GetStaticProps, InferGetStaticPropsType} from 'next';
-import Head from 'next/head';
-import {BlogPost} from '../@types/schema';
-import BlogCard from '../components/BlogCard';
-import NotionService from '../services/notion-service';
-import Header from '../components/header';
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import Head from 'next/head'
+import { BlogPost } from '../@types/schema'
+import BlogCard from '../components/BlogCard'
+import NotionService from '../services/notion-service'
+import Header from '../components/header'
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const notionService = new NotionService();
-  const posts = await notionService.getPublishedBlogPosts();
+  const notionService = new NotionService()
+  const posts = await notionService.getPublishedBlogPosts()
 
   return {
     props: {
       posts,
     },
-  };
-};
+  }
+}
 
-const Home = ({posts}: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const title = 'WISE TECH';
-  const description = '와이즈인 컴퍼니 기술블로그';
+const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const title = 'WISE TECH'
+  const description = '와이즈인 컴퍼니 기술블로그'
 
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta
-          name={'description'}
-          title={'description'}
-          content={description}
-        />
-        <meta
-          name={'og:title'}
-          title={'og:title'}
-          content={title}
-        />
-        <meta
-          name={'og:description'}
-          title={'og:description'}
-          content={title}
-        />
+        <meta name={'description'} title={'description'} content={description} />
+        <meta name={'og:title'} title={'og:title'} content={title} />
+        <meta name={'og:description'} title={'og:description'} content={title} />
       </Head>
 
-      <div className='min-h-screen'>
-        <main className='max-w-5xl mx-auto relative'>
-          <div className='h-full pt-4 pb-16 mx-auto'>
+      <div className="min-h-screen">
+        <main className="max-w-5xl mx-auto relative">
+          <div className="h-full pt-4 pb-16 mx-auto">
             <Header />
-            <div className='mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 lg:max-w-none'>
+            <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 lg:max-w-none">
               {posts.map((post: BlogPost) => (
-                <BlogCard
-                  key={post.id}
-                  post={post}
-                />
+                <BlogCard key={post.id} post={post} />
               ))}
             </div>
           </div>
         </main>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
