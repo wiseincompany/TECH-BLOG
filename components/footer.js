5d24145fa00490282448d30696b8c73d2e4fdf10
GitHubJIYEON <jiyeon2954@gmail.com>
2023-04-14 15:08:01 +0900

    header footer

--- components/footer.js
+++ components/footer.js
@@ -5,20 +5,20 @@ import Logo from '../public/images/main_footer-logo.png'
 export default function Footer() {
   return (
     <>
-      <footer className="text-gray-600 body-font">
+      <footer className="text-gray-600 body-font pt-4">
         <div className="bg-gray-100">
-          <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+          <div className="container flex justify-between p-8  mx-auto items-center sm:flex-row flex-col">
             <Link href="/" legacyBehavior>
               <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                <Image src={Logo} alt="logo footer" />
+                <Image src={Logo} alt="logo footer" width={84} height={80} />
               </a>
             </Link>
 
-            <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
+            <p className="text-sm text-gray-500 ">
               © 2023 WISEINCOMPANY — ALL RIGHTS RESERVED.
               <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank"></a>
             </p>
-            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+            <span className="inline-flex justify-center sm:justify-start">
               <a className="text-gray-500">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
