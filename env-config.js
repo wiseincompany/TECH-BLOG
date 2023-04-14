7db9ee56d852e928d38e4b1a769df352fa4359c7
Returnmakdo <cldud970@naver.com>
2023-04-14 09:58:11 +0900

    배포.

--- env-config.js
+++
@@ -1,6 +0,0 @@
-const debug = process.env.NODE_ENV !== 'production'
-const name = 'Tech-Blog'
-
-module.exports = {
-  'process.env.BACKEND_URL': !debug ? `/${name}` : '',
-}
--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== 'production'
-const name = 'Tech-Blog'
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: !debug ? `/${name}/` : '',
-  trailingSlash: true,
 }
