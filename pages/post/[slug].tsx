aa7438d9d4ac353b2c99e224162ccc8e39b917f1
Returnmakdo <cldud970@naver.com>
2023-04-17 13:24:11 +0900

    style: 이미지 수정

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -9,9 +9,10 @@ import remarkGfm from 'remark-gfm'
 import remarkParse from 'remark-parse/lib'
 import { unified } from 'unified'
 import Image from 'next/image'
+import '../../styles/Home.module.css'
 
 const Post = ({ markdown, post }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const result = markdown.replace(/img/g, 'img style="margin: auto"')
+  const result = markdown.replace(/img/g, 'img style="margin: auto; object-fit: cover; image-rendering: -webkit-optimize-contrast; transform: translateZ(0); backface-visibility: hidden; "')
   return (
     <>
       <Head>
@@ -28,7 +29,7 @@ const Post = ({ markdown, post }: InferGetStaticPropsType<typeof getStaticProps>
             <h1 className="text-4xl font-bold flex items-center justify-center mb-10">{post.title}</h1>
             <h4 className="text-s text-center font-medium mb-10 text-gray-400">{post.date.substr(0, 10)}</h4>
             <div className="text-center mb-20">
-              <Image className="rounded-xl" objectFit="cover" src={post.cover} alt="" width={700} height={600} />
+              <Image className="rounded-xl align-top card__image" objectFit="contain" src={post.cover} alt="" width={700} height={500} quality={100} />
             </div>
             <div className="flex items-center justify-center">
               <article className="prose w-[100%]" dangerouslySetInnerHTML={{ __html: result }}></article>
@@ -42,6 +43,11 @@ const Post = ({ markdown, post }: InferGetStaticPropsType<typeof getStaticProps>
             </span>
           </main>
         </div>
+        <style jsx>{`
+          .card__image {
+            width: 100%;
+          }
+        `}</style>
       </Layout>
     </>
   )
